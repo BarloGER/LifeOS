950c687b01572bd66d38f95bbb570193c9101fc5
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-03-11 12:27:07 +0100

    Server: Added DB connection

---
+++ server/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { ErrorResponse } from "../utils/ErrorResponse.js";
+
+export const connectToDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    const client = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Connected to MongoDB @ ${client.connection.host}`);
+    return client;
+  } catch (error) {
+    throw new ErrorResponse({
+      message: "Fehler bei der Verbindung zur Datenbank.",
+      statusCode: 500,
+      errorType: "DatabaseError",
+      errorCode: "DB_001",
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
+import { connectToDB } from "./db/index.js";
 
 const app = express();
 const PORT = 8080 || process.env.PORT;
+connectToDB();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // extended: true to get rid of body-parser error
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
