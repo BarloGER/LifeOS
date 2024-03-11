538f8a991c23a850ea1c1cfb29f2c97688d787f0
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-03-11 15:51:59 +0100

    Server: Added authRouter and used in in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
+import { errorHandler } from "./middlewares/errorHandler.js";
 import { connectToDB } from "./db/index.js";
+import { authRouter } from "./routes/authRouter.js";
 
 const app = express();
 const PORT = 8080 || process.env.PORT;
@@ -10,9 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // extended: true to get rid of body-parser error
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/auth", authRouter);
+app.use("*", (req, res) => res.sendStatus(404));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
