a674dc819e7b6ccd70101116b12d84bfecb617b1
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-03-16 21:16:17 +0100

    Client: Added test dashboard for routing

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import "../assets/dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <section className="dashboard-container">
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
