29d9f500152205691c45f93407a1e146c95c0dd2
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-03-16 20:05:29 +0100

    Client: Added ependencies, global.css and .env to .gitignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,75 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+import { useAuth } from "./features/authentication";
+
+import GlobalLayout from "./layouts/GlobalLayout";
+import { ProtectedRoutes } from "./features/authentication";
+
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import ShoppingListDetails from "./features/shopping-list/components/ShoppingListDetails";
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  const {
+    setToken,
+    isAuthenticated,
+    setIsAuthenticated,
+    loadingAuthRequest,
+    setLoadingAuthRequest,
+  } = useAuth();
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<GlobalLayout />}>
+        <Route
+          index
+          element={
+            <SignIn
+              setToken={setToken}
+              isAuthenticated={isAuthenticated}
+              loadingAuthRequest={loadingAuthRequest}
+              setLoadingAuthRequest={setLoadingAuthRequest}
+            />
+          }
+        />
+        <Route
+          path="/sign-up"
+          element={
+            <SignUp
+              setToken={setToken}
+              isAuthenticated={isAuthenticated}
+              setIsAuthenticated={setIsAuthenticated}
+              loadingAuthRequest={loadingAuthRequest}
+              setLoadingAuthRequest={setLoadingAuthRequest}
+            />
+          }
+        />
+
+        <Route
+          path="/auth"
+          element={
+            <ProtectedRoutes
+              isAuthenticated={isAuthenticated}
+              loadingAuthRequest={loadingAuthRequest}
+            />
+          }
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route
+            path="shopping-lists/:listId"
+            element={<ShoppingListDetails />}
+          />
+        </Route>
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
