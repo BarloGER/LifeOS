d6cfbc77ea9a72427e42f2f0e6b7bc37db0e2ea6
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-04-10 11:20:15 +0200

    Server: Added DB transaction for user deletion and renamed friend transaction

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
 import { validateJoi } from "../middlewares/validateJoi.js";
+import { deleteUserTransaction } from "../middlewares/deleteUserTransaction.js";
 import { userSchema } from "../joi/userSchema.js";
 import {
   getUser,
@@ -13,6 +14,6 @@ export const userRouter = Router();
 
 userRouter.get("/me", verifyToken, getUser);
 userRouter.put("/me", verifyToken, validateJoi(userSchema), editUser);
-userRouter.delete("/me", verifyToken, deleteUser);
+userRouter.delete("/me", verifyToken, deleteUserTransaction, deleteUser);
 
 userRouter.get("/users", verifyToken, getUserByUsername);
