211ef1f1157b77e1fd35fb78cdb50f0ba8b64985
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-04-12 00:44:32 +0200

    Server: Added getLockStatus to heartbeatController and Router

--- server/routes/heartbeatRouter.js
+++ server/routes/heartbeatRouter.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
 import { validateJoi } from "../middlewares/validateJoi.js";
 import {
+  getLockStatusSchema,
   sendHeartbeatSchema,
   lockFeatureEditSchema,
   unlockFeatureEditSchema,
 } from "../joi/heartbeatSchemas.js";
 import {
+  getLockStatus,
   sendHeartbeat,
   unlockFeatureEdit,
   lockFeatureEdit,
@@ -14,18 +16,26 @@ import {
 
 export const heartbeatRouter = Router();
 
+heartbeatRouter.post(
+  "/heartbeat/:featureID",
+  verifyToken,
+  validateJoi(getLockStatusSchema),
+  getLockStatus,
+);
 heartbeatRouter.put(
   "/heartbeat/:featureID",
   verifyToken,
   validateJoi(sendHeartbeatSchema),
   sendHeartbeat,
 );
+
 heartbeatRouter.post(
   "/heartbeat/lock/:featureID",
   verifyToken,
   validateJoi(lockFeatureEditSchema),
   lockFeatureEdit,
 );
+
 heartbeatRouter.post(
   "/heartbeat/unlock/:featureID",
   verifyToken,
