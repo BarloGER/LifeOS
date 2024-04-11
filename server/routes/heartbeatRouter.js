29bddae02926f50e48a86e7b834f001af679b3c8
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-04-11 20:56:19 +0200

    Server: Added heartbeatSchemas

---
+++ server/routes/heartbeatRouter.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { verifyToken } from "../middlewares/verifyToken.js";
+import { validateJoi } from "../middlewares/validateJoi.js";
+import {
+  sendHeartbeatSchema,
+  lockFeatureEditSchema,
+  unlockFeatureEditSchema,
+} from "../joi/heartbeatSchemas.js";
+import {
+  sendHeartbeat,
+  unlockFeatureEdit,
+  lockFeatureEdit,
+} from "../controllers/heartbeatController.js";
+
+export const heartbeatRouter = Router();
+
+heartbeatRouter.put(
+  "/heartbeat/:featureID",
+  verifyToken,
+  validateJoi(sendHeartbeatSchema),
+  sendHeartbeat,
+);
+heartbeatRouter.post(
+  "/heartbeat/lock/:featureID",
+  verifyToken,
+  validateJoi(lockFeatureEditSchema),
+  lockFeatureEdit,
+);
+heartbeatRouter.post(
+  "/heartbeat/unlock/:featureID",
+  verifyToken,
+  validateJoi(unlockFeatureEditSchema),
+  unlockFeatureEdit,
+);
